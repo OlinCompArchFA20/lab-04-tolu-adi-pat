f244ed212c1e9e78ca64d831ff27d9655bf58d4c
adi_ramachandran <aramachandran@olin.edu>
2020-11-16 19:35:32 +0000

    got jump working

--- verilog/fetch.v
+++ verilog/fetch.v
@@ -20,7 +20,9 @@ module FETCH
     else begin
       case(pc_src)
         `PC_SRC_NEXT: begin pc_next = pc_next + `W_CPU'b100; end
-        `PC_SRC_JUMP: begin pc_next = jump_addr; end
+        `PC_SRC_JUMP: begin pc_next = {pc_next[`PC_UPPER], jump_addr, `W_WORD'b0}; end
+
+
         `PC_SRC_BRCH: begin  if (branch_ctrl == 1)  pc_next = imm_addr;
                      else pc_next = pc_next + 4; end
         `PC_SRC_REGF: begin pc_next = reg_addr; end
